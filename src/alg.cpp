1fc3455f84272355192ea647cd02fd7eb66fca30
lisyonkovaa <114025777+lisyonkovaa@users.noreply.github.com>
2023-03-22 11:41:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,50 @@
-// Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+    int count = 0;
+    for (int i = 0; i < len; ++i) {
+        for (int j = i + 1; j < len; ++j) {
+            if (arr[i] + arr[j] == value) {
+                ++count;
+            }
+        }
+    }
+    return count;
 }
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+    int count = 0;
+    int left = 0;
+    int right = len - 1;
+    while (left < right) {
+        int sum = arr[left] + arr[right];
+        if (sum == value) {
+            ++count;
+            while (left < right && arr[left] == arr[left + 1]) {
+                ++left;
+            }
+            while (left < right && arr[right] == arr[right - 1]) {
+                --right;
+            }
+            ++left;
+            --right;
+        } else if (sum < value) {
+            ++left;
+        } else {
+            --right;
+        }
+    }
+    return count;
 }
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+    int count = 0;
+    for (int i = 0; i < len; ++i) {
+        int diff = value - arr[i];
+        int* found = std::lower_bound(arr + i + 1, arr + len, diff);
+        if (found != arr + len && *found == diff) {
+            ++count;
+            while (found + 1 < arr + len && *(found + 1) == diff) {
+                ++found;
+                ++count;
+            }
+        }
+    }
+    return count;
 }
